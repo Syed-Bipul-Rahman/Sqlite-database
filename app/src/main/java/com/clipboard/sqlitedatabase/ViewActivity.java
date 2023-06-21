131603db316503b46fb5c32569984fe74a7187dc
Sb-Rahman <sb.rahman@yahoo.com>
2023-06-21 22:47:57 +0600

    ViewData Activity added and nessesery code implemented

--- app/src/main/java/com/clipboard/sqlitedatabase/ViewActivity.java
+++ app/src/main/java/com/clipboard/sqlitedatabase/ViewActivity.java
@@ -3,12 +3,24 @@ package com.clipboard.sqlitedatabase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ViewActivity extends AppCompatActivity {
+    TextView tv;
+    DatabaseHandler db;
+    String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+
+        tv=findViewById(R.id.viewdata);
+
+        db=new DatabaseHandler(this);
+        data=db.getData();
+        tv.setText(data);
+
+
     }
 }
