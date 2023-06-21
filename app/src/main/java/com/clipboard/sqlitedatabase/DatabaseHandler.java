d6bcc8d59b5da86d0f353b4490f58ad7e16047d2
Sb-Rahman <sb.rahman@yahoo.com>
2023-06-21 19:33:54 +0600

    DatabaseHandler updated

--- app/src/main/java/com/clipboard/sqlitedatabase/DatabaseHandler.java
+++ app/src/main/java/com/clipboard/sqlitedatabase/DatabaseHandler.java
@@ -57,8 +57,8 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         return db.insert(DATABASE_TABLE, null, values);
     }
 
-    //get single employee
-    public String getEmployee() {
+    //get single data
+    public String getData() {
 
         db = this.getWritableDatabase();
 
@@ -75,13 +75,65 @@ public class DatabaseHandler extends SQLiteOpenHelper {
                     "id: " + cursor.getString(eId) + "\n" +
                     "Name: " + cursor.getString(eName) + "\n" +
                     "Age: " + cursor.getString(eAge) + "\n" +
-                    "City: " + cursor.getString(eCity) + "\n";
+                    "City: " + cursor.getString(eCity) + "\n\n";
         }
         db.close();
         return res;
     }
 
+    //update single data
+    public void updateData(long l, String name, String age, String city) {
+        db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(DATABASE_KEY_NAME, name);
+        values.put(DATABASE_KEY_AGE, age);
+        values.put(DATABASE_KEY_CITY, city);
+
+        db.update(DATABASE_TABLE, values, DATABASE_KEY_ID + "=" + l, null);
+        db.close();
 
 
+    }
+
+    //deleting single data
+    public void deleteData(long l) {
+        db = this.getWritableDatabase();
+        db.delete(DATABASE_TABLE, DATABASE_KEY_ID + "=" + l, null);
+    }
+
+    public String getName(long l1) {
+        db = this.getWritableDatabase();
+        Cursor cursor = db.query(DATABASE_TABLE, new String[]{DATABASE_KEY_ID, DATABASE_KEY_NAME, DATABASE_KEY_AGE, DATABASE_KEY_CITY}, DATABASE_KEY_ID + "=" + l1, null, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            String name = cursor.getString(1);
+            return name;
+        }
+        return null;
+    }
+
+    public String getAge(long l1) {
+        db = this.getWritableDatabase();
+        Cursor cursor = db.query(DATABASE_TABLE, new String[]{DATABASE_KEY_ID, DATABASE_KEY_NAME, DATABASE_KEY_AGE, DATABASE_KEY_CITY}, DATABASE_KEY_ID + "=" + l1, null, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            String age = cursor.getString(2);
+            return age;
+        }
+        return null;
+    }public String getCity(long l1) {
+        db = this.getWritableDatabase();
+        Cursor cursor = db.query(DATABASE_TABLE, new String[]{DATABASE_KEY_ID, DATABASE_KEY_NAME, DATABASE_KEY_AGE, DATABASE_KEY_CITY}, DATABASE_KEY_ID + "=" + l1, null, null, null, null);
+
+        if (cursor != null) {
+            cursor.moveToFirst();
+            String city = cursor.getString(3);
+            return city;
+        }
+        return null;
+    }
 
 }
