890fe270a2324f3b4251f51be96e8fe1e35a55db
Sb-Rahman <sb.rahman@yahoo.com>
2023-06-21 17:33:18 +0600

    Main layout design added

---
+++ app/src/main/java/com/clipboard/sqlitedatabase/MainActivity.java
@@ -0,0 +1,14 @@
+package com.clipboard.sqlitedatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
